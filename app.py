21d6c9a930cd631dfcfc361c9a1422e8d8110b8e
zeljko00 <tripic.zeljko@gmail.com>
2023-08-14 22:10:52 +0200

    login and register func

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import json
+import auth
+import time
 
 conf_path="app_conf.json"
+username_label="username"
+password_label="password"
+server_url="server_url"
+auth_interval="auth_interval"
 
 # reading app configuration from json file
 def read_conf():
@@ -10,3 +16,30 @@ def read_conf():
         return conf
     except:
         return None
+def signup_periodically(username,password,url,interval):
+    jwt=None
+    while jwt==None:
+        jwt=auth.register(username,password,url)
+        time.sleep(interval)
+    return jwt
+
+def handle_data(jwt):
+    print("handling data")
+
+def main():
+    #read app config
+    config=read_conf()
+    #if config is read successfully, start app logic
+    if config!=None:
+        #iot cloud platform login
+        jwt=auth.login(config[username_label],config[password_label],config[server_url])
+        #if failed, periodically request signup
+        if jwt==None:
+            jwt=signup_periodically(config[username_label],config[password_label],config[server_url],config[auth_interval])
+        #now JWT required for Cloud platform authorization is stored in jwt var
+        handle_data(jwt)
+    else:
+        print("Can't read app config file!")
+
+if __name__ == '__main__':
+    main()
--- auth.py
+++ auth.py
@@ -1,12 +1,24 @@
 import requests
 import base64
 
-def login(username,password,register,url):
-    # creating base64 encoded form of username:password for basih auth
+def login(username,password,url):
+    # creating base64 encoded username:password token for basic auth
     basic_auth="Basic "+(base64.b64encode((username+":"+password).encode("ascii")).decode("ascii"))
-    print(basic_auth)
-    login_req=requests.get(url,
-                           headers={"Authorization":basic_auth})
-    print(login_req.status_code)
+    try:
+        login_req=requests.get(url+"/auth/login",
+                               headers={"Authorization":basic_auth})
+        # print("Login status: ",login_req.status_code)
+        # print("Login response: ",login_req.text)
+        return login_req.text if login_req.status_code==200 else None
+    except:
+        return None
 
-login("zex","pass",True,"")
+def register(username,password,url):
+    try:
+        login_req=requests.post(url+"/auth/signup",
+                               params={"username":username,"password": password})
+        # print("Signup status: ",login_req.status_code)
+        # print("Signup response: ",login_req.text)
+        return (login_req.text if login_req.status_code == 200 else None)
+    except:
+        return None
