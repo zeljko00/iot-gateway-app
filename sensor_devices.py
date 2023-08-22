f92b9653af77df0fce023456ca7b05a4d1703993
zeljko00 <tripic.zeljko@gmail.com>
2023-08-22 07:55:35 +0200

    sending sensor data to local mqtt broker

--- sensor_devices.py
+++ sensor_devices.py
@@ -2,6 +2,7 @@ import time
 import random
 import numpy
 import json
+import paho.mqtt.client as mqtt
 from multiprocessing import Process, Event
 import logging.config
 
@@ -26,23 +27,26 @@ time_format = "%d.%m.%Y.  %H:%M:%S"
 celzius = "C"
 kg = "kg"
 liter = "l"
-
-
-class SensorData:
-    def __init__(self, value, time, unit):
-        self.value = value
-        self.time = time
-        self.unit = unit
+mqtt_broker="mqtt_broker"
+address="address"
+port="port"
+transport_protocol="tcp"
+temp_topic="sensors/temperature"
+load_topic="sensors/arm-load"
+fuel_topic="sensors/fuel-level"
 
 
 # period = measuring interval in sec, min_val/max_val = min/max measured value
-def measure_temperature_periodically(period, min_val, max_val, flag, stub):
+def measure_temperature_periodically(period, min_val, max_val, broker_address, broker_port, flag):
     print("Temperature sensor conf: interval={}s , min={}˚C , max={}C".format(period, min_val,max_val))
     print("------------------------------------------------------")
     # prevent division by zero
     if period == 0:
         period = 1
     period = abs(round(period))
+    # initializing mqtt client
+    client = mqtt.Client(client_id="temp-sensor-mqtt-client", transport=transport_protocol, protocol=mqtt.MQTTv5)
+    client.connect(broker_address, port=broker_port, keepalive=period*3)
     # provide sensor with data for 7 days
     values_count = round(7 * 24 * 60 * 60 / period)
     data = numpy.random.uniform(min_val, max_val, values_count)
@@ -50,20 +54,22 @@ def measure_temperature_periodically(period, min_val, max_val, flag, stub):
     # shut down sensor depending on set flag
     while not flag.is_set():
         time.sleep(period)
-        # create object representing data
-        measured_value = SensorData(data[counter % values_count], time.strftime(time_format, time.localtime()), celzius)
-        # send data to iot gateway
-        # testing1
-        stub.put(measured_value)
-        # testing2
-        # stub.append(measured_value)
-        # print(stub)
+        # check connection to mqtt broker
+        while not client.is_connected():
+            errorLogger.error("Temperature sensor lost connection to MQTT broker!")
+            client.reconnect()
+        try:
+            # send data to MQTT broker
+            client.publish(temp_topic, str(data[counter % values_count])+"#"+str(time.strftime(time_format, time.localtime()))+"#"+celzius)
+        except:
+            errorLogger.error("Temperature sensor can't connect to MQTT broker!")
         counter += 1
+    client.disconnect()
     print("Temperature sensor shutdown!")
 
 
 # min_t/max_t = min/max measuring period in sec, min_val/max_val = min/max measured value
-def measure_weight_randomly(min_t, max_t, min_val, max_val,flag,stub):
+def measure_load_randomly(min_t, max_t, min_val, max_val, broker_address, broker_port, flag):
     print("Arm sensor conf: min_interval={}s , max_interval={}s , min={}kg , max={}kg".format(min_t, max_t, min_val, max_val))
     print("------------------------------------------------------")
     # parameter validation
@@ -71,6 +77,9 @@ def measure_weight_randomly(min_t, max_t, min_val, max_val,flag,stub):
         max_t = min_t + random.randint(10)
     min_t = abs(round(min_t))
     max_t = abs(round(max_t))
+    # initializing mqtt client
+    client = mqtt.Client(client_id="arm-load-sensor-mqtt-client", transport=transport_protocol, protocol=mqtt.MQTTv5)
+    client.connect(broker_address, port=broker_port, keepalive=max_t*3)
     # provide sensor with data for at least 7 days
     values_count = round(7 * 24 * 60 * 60 / min_t)
     intervals = numpy.random.uniform(min_t, max_t, values_count)
@@ -79,21 +88,23 @@ def measure_weight_randomly(min_t, max_t, min_val, max_val,flag,stub):
     # shut down sensor depending on set flag
     while not flag.is_set():
         time.sleep(round(intervals[counter % values_count]))
-        # create object representing data
-        measured_value = SensorData(data[counter % values_count], time.strftime(time_format, time.localtime()), kg)
-        # send data to iot gateway
-        # testing1
-        stub.put(measured_value)
-        # testing2
-        # stub.append(measured_value)
-        # print(stub)
+        # check connection to mqtt broker
+        while not client.is_connected():
+            errorLogger.error("Arm load sensor lost connection to MQTT broker!")
+            client.reconnect()
+        try:
+            # send data to MQTT broker
+            client.publish(load_topic, str(data[counter % values_count])+"#"+str(time.strftime(time_format, time.localtime()))+"#"+kg)
+        except:
+            errorLogger.error("Arm load sensor can't connect to MQTT broker!")
         counter += 1
+    client.disconnect()
     print("Arm load sensor shutdown!")
 
 
 # period = measuring interval , capacity = fuel tank capacity , refill = fuel tank refill probability (0-1)
 # consumption = fuel usage consumption per working hour, efficiency = machine work efficiency (0-1)
-def measure_fuel_periodically(period, capacity, consumption, efficiency, refill, flag, stub):
+def measure_fuel_periodically(period, capacity, consumption, efficiency, refill, broker_address, broker_port, flag):
     print("Fuel sensor conf: period={}s , capacity={}l , consumption={}l/h , efficiency={} , refill={}".
           format(period, capacity, consumption, efficiency, refill))
     print("------------------------------------------------------")
@@ -101,6 +112,9 @@ def measure_fuel_periodically(period, capacity, consumption, efficiency, refill,
     if period == 0:
         period = 1
     period = abs(round(period))
+    # initializing mqtt client
+    client = mqtt.Client(client_id="fuel-sensor-mqtt-client", transport=transport_protocol, protocol=mqtt.MQTTv5)
+    client.connect(broker_address, port=broker_port, keepalive=period*3)
     # at first fuel tank is randomly filled
     value = random.randint(round(capacity / 2), round(capacity))
     # constant for scaling consumption per hour to per second
@@ -123,14 +137,17 @@ def measure_fuel_periodically(period, capacity, consumption, efficiency, refill,
             if value <= 0:
                 value = 0
                 refilling = True
-        # create object representing data
-        measured_value = SensorData(value, time.strftime(time_format, time.localtime()),liter)
-        # send data to iot gateway
-        # testing1
-        stub.put(measured_value)
-        # testing2
-        # stub.append(measured_value)
-        # print(stub)
+        # check connection to mqtt broker
+        while not client.is_connected():
+            errorLogger.error("Fuel level sensor lost connection to MQTT broker!")
+            client.reconnect()
+        try:
+            # send data to MQTT broker
+            client.publish(fuel_topic,
+                           str(value) + "#" + str(time.strftime(time_format, time.localtime())) + "#" + liter)
+        except:
+            errorLogger.error("Fuel level sensor can't connect to MQTT broker!")
+    client.disconnect()
     print("Fuel level sensor shutdown!")
 
 
@@ -145,7 +162,8 @@ def read_conf():
         data = {temp_sensor: {interval: 5, min: -10, max: 100},
                 arm_sensor: {arm_min_t: 10, arm_max_t: 100, min: 0, max: 800},
                 fuel_sensor: {interval: 5, fuel_capacity: 300, fuel_consumption: 3000, fuel_efficiency: 0.6,
-                              fuel_refill: 0.02}}
+                              fuel_refill: 0.02},
+                mqtt_broker: { address: "localhost", port:1883}}
     finally:
         print("Sensors config:")
         print(data)
@@ -159,17 +177,24 @@ def sensors_devices(temp_flag, load_flag, fuel_flag):
     temperature_sensor = Process(target=measure_temperature_periodically, args=(conf_data[temp_sensor][interval],
                                                                                 conf_data[temp_sensor][min],
                                                                                 conf_data[temp_sensor][max],
-                                                                                temp_flag, []))
-    excavator_arm_sensor = Process(target=measure_weight_randomly, args=(conf_data[arm_sensor][arm_min_t],
+                                                                                conf_data[mqtt_broker][address],
+                                                                                conf_data[mqtt_broker][port],
+                                                                                temp_flag, ))
+    excavator_arm_sensor = Process(target=measure_load_randomly, args=(conf_data[arm_sensor][arm_min_t],
                                                                          conf_data[arm_sensor][arm_max_t],
                                                                          conf_data[arm_sensor][min],
-                                                                         conf_data[arm_sensor][max], load_flag, []))
+                                                                         conf_data[arm_sensor][max],
+                                                                         conf_data[mqtt_broker][address],
+                                                                         conf_data[mqtt_broker][port],
+                                                                         load_flag,))
     fuel_level_sensor = Process(target=measure_fuel_periodically, args=(conf_data[fuel_sensor][interval],
                                                                         conf_data[fuel_sensor][fuel_capacity],
                                                                         conf_data[fuel_sensor][fuel_consumption],
                                                                         conf_data[fuel_sensor][fuel_efficiency],
                                                                         conf_data[fuel_sensor][fuel_refill],
-                                                                        fuel_flag, []))
+                                                                        conf_data[mqtt_broker][address],
+                                                                        conf_data[mqtt_broker][port],
+                                                                        fuel_flag,))
     return [temperature_sensor, excavator_arm_sensor, fuel_level_sensor]
 
 
@@ -192,29 +217,6 @@ def main():
     for sensor in sensors:
         sensor.join()
     infoLogger.info("Sensor system shutdown!")
-
-
-def test(temp,load,fuel):
-    conf_data = read_conf()
-    temperature_sensor = Process(target=measure_temperature_periodically, args=(conf_data[temp_sensor][interval],
-                                                                                conf_data[temp_sensor][min],
-                                                                                conf_data[temp_sensor][max],
-                                                                                Event(), temp))
-    excavator_arm_sensor = Process(target=measure_weight_randomly, args=(conf_data[arm_sensor][arm_min_t],
-                                                                         conf_data[arm_sensor][arm_max_t],
-                                                                         conf_data[arm_sensor][min],
-                                                                         conf_data[arm_sensor][max], Event(), load))
-    fuel_level_sensor = Process(target=measure_fuel_periodically, args=(conf_data[fuel_sensor][interval],
-                                                                        conf_data[fuel_sensor][fuel_capacity],
-                                                                        conf_data[fuel_sensor][fuel_consumption],
-                                                                        conf_data[fuel_sensor][fuel_efficiency],
-                                                                        conf_data[fuel_sensor][fuel_refill],
-                                                                        Event(), fuel))
-    temperature_sensor.start()
-    time.sleep(0.1)
-    excavator_arm_sensor.start()
-    time.sleep(0.1)
-    fuel_level_sensor.start()
 
 if __name__ == '__main__':
     main()
