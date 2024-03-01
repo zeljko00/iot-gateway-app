734dbc7765dfa2f3f54d77962dc9e3d278b004a2
Stefan Kapetanovic <stekap000@gmail.com>
2024-03-01 14:07:43 +0100

    rest_api added docstrings and removed unnecessary imports

--- src/rest_api.py
+++ src/rest_api.py
@@ -1,9 +1,7 @@
-import contextlib
-import time
-import threading
+"""Simple gateway rest api server."""
 import uvicorn
 import logging.config
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
@@ -19,27 +17,88 @@ customLogger = logging.getLogger('customConsoleLogger')
 
 
 class FuelSettings(BaseModel):
+    """Class representing fuel settings that can be received.
+
+    Attributes
+    ----------
+    level_limit: int
+       Fuel level limit.
+    mode: str
+       Fuel sensor mode of operation.
+
+    """
+
     level_limit: int
     mode: str
 
 
 class TempSettings(BaseModel):
+    """Class representing temp settings that can be received.
+
+    Attributes
+    ----------
+    interval: int
+       Temperature interval.
+    mode: str
+       Temperature sensor mode of operation.
+
+    """
+
     interval: int
     mode: str
 
 
 class LoadSettings(BaseModel):
+    """Class representing load settings that can be received.
+
+    Attributes
+    ----------
+    interval: int
+       Load interval.
+    mode: str
+       Load sensor mode of operation.
+
+    """
+
     interval: int
     mode: str
 
 
 class FluidConfig(BaseModel):
+    """Wrapper class for all configuration options that are subject to change.
+
+    This class is used for the possible case where transferred information contains
+    parts subject to change and those that must not change.
+
+    Attributes
+    ----------
+    fuel_settings: FuelSettings
+       Fuel settings.
+    temp_settings: TempSettings
+       Fuel settings.
+    load_settings: LoadSettings
+       Fuel settings.
+
+    """
+    
     fuel_settings: FuelSettings
     temp_settings: TempSettings
     load_settings: LoadSettings
 
 
 def start_rest_api(host, port):
+    """Start gateway rest api server.
+
+    Setup properties and endpoints and start simple gateway rest api server.
+
+    Parameters
+    ----------
+    host: str
+       Rest api host.
+    port: int
+       Rest api port.
+
+    """
     app = FastAPI()
 
     origins = ["*"]
